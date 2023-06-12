216c666f9f8c35f7d0d90bdae487c7bc90fcaf30
Ismail Saleh <centriu78@gmail.com>
2023-06-12 23:02:40 +0300

    dsa

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,38 +1,29 @@
 <template>
   <v-footer bottom absolute app theme="dark">
-    <v-col cols="12" class="text-center">
+    <v-col cols="12" sm="4" align="center" justify="center">
       <v-row align="center" justify="center">
-        <v-col cols="12" sm="4" align="center" justify="center">
-          <v-row align="center" justify="center">
-            <v-btn
-              outlined
-              tile
-              small
-              text
-              @click="openDoc('terms-and-conditions')"
-              >Общи Условия</v-btn
-            >
-          </v-row>
-        </v-col>
-        <v-col
-          cols="12"
-          sm="4"
-          align="center"
-          justify="center"
-          class="text-center"
-          style="font-size: 0.8rem"
+        <v-btn outlined tile small text @click="openDoc('terms-and-conditions')"
+          >Общи Условия</v-btn
         >
-          &#169; {{ new Date().getFullYear() }} -
-          <strong>Исмаил Салех @ ТУ-София</strong>
-          Всички права запазени.
-        </v-col>
-        <v-col cols="12" sm="4" align="center" justify="center">
-          <v-row align="center" justify="center">
-            <v-btn outlined tile small text @click="openDoc('contact-info')"
-              >Поддръжка</v-btn
-            >
-          </v-row>
-        </v-col>
+      </v-row>
+    </v-col>
+    <v-col
+      cols="12"
+      sm="4"
+      align="center"
+      justify="center"
+      class="text-center"
+      style="font-size: 0.8rem"
+    >
+      &#169; {{ new Date().getFullYear() }} -
+      <strong>Исмаил Салех @ ТУ-София</strong>
+      Всички права запазени.
+    </v-col>
+    <v-col cols="12" sm="4" align="center" justify="center">
+      <v-row align="center" justify="center">
+        <v-btn outlined tile small text @click="openDoc('contact-info')"
+          >Поддръжка</v-btn
+        >
       </v-row>
     </v-col>
   </v-footer>
