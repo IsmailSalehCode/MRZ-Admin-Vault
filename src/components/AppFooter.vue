6ed39de2a82983073e4c51e36bfaf19ccd1c9b88
Ismail Saleh <centriu78@gmail.com>
2023-06-13 00:02:02 +0300

    styling

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,28 +1,24 @@
 <template>
   <v-footer bottom absolute app theme="dark">
-    <v-col cols="12" sm="4" align="center" justify="center">
-      <v-row align="center" justify="center">
+    <v-col style="text-align: center">
+      <v-row align="center" justify="space-evenly">
         <v-btn
           variant="outlined"
           size="small"
           @click="openDoc('terms-and-conditions')"
           >Общи Условия</v-btn
         >
-      </v-row>
-    </v-col>
-    <v-col
-      cols="12"
-      sm="4"
-      align="center"
-      justify="center"
-      class="text-center"
-      style="font-size: 0.8rem"
-    >
-      &#169; 2023 -
-      <strong>Исмаил Салех @ ТУ-София</strong>
-    </v-col>
-    <v-col cols="12" sm="4" align="center" justify="center">
-      <v-row align="center" justify="center">
+        <v-col
+          cols="12"
+          sm="4"
+          align="center"
+          justify="center"
+          class="text-center"
+          style="font-size: 0.8rem"
+        >
+          &#169; 2023 -
+          <strong>Исмаил Салех @ ТУ-София</strong>
+        </v-col>
         <v-btn variant="outlined" size="small" @click="openDoc('contact-info')"
           >Поддръжка</v-btn
         >
