8681bbb8e41d4d06ca1c6ce8eeeb4395c1e764a6
Ismail Saleh <centriu78@gmail.com>
2023-06-13 03:44:52 +0300

    BG context actions

--- electron/electron.js
+++ electron/electron.js
@@ -20,9 +20,9 @@ function createWindow() {
   mainWindow.webContents.on("context-menu", (_, props) => {
     const menu = new Menu();
     if (props.isEditable) {
-      menu.append(new MenuItem({ label: "Cut", role: "cut" }));
-      menu.append(new MenuItem({ label: "Copy", role: "copy" }));
-      menu.append(new MenuItem({ label: "Paste", role: "paste" }));
+      menu.append(new MenuItem({ label: "Изрежи", role: "cut" }));
+      menu.append(new MenuItem({ label: "Копирай", role: "copy" }));
+      menu.append(new MenuItem({ label: "Постави", role: "paste" }));
       menu.popup();
     }
   });
