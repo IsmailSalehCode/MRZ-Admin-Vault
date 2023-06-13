f425bcd30c3454f26666d6fa7e999121682d8898
Ismail Saleh <centriu78@gmail.com>
2023-06-13 03:05:25 +0300

    Electron scaffold

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.ELECTRON == "true" ? "./" : ".",
   plugins: [
     vue({
       template: { transformAssetUrls },
