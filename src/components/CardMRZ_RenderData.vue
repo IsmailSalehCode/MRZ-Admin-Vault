15c7715dd0860430f9bdff0d4e42ec58e212785f
Ismail Saleh <centriu78@gmail.com>
2023-06-15 03:18:57 +0300

    added card subtitle

--- src/components/CardMRZ_RenderData.vue
+++ src/components/CardMRZ_RenderData.vue
@@ -2,6 +2,7 @@
   <div>{{ JSON.stringify(rdata) }}</div>
   <v-card max-width="740px" variant="outlined" rounded="xl">
     <v-card-title>{{ mrzData.surname }}, {{ mrzData.givenNames }}</v-card-title>
+    <v-card-subtitle>Фамилия, дадени имена</v-card-subtitle>
   </v-card>
 </template>
 <script>
