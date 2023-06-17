d9403c2005d796c5add692df72913d5d5e52cbb4
Ismail Saleh <centriu78@gmail.com>
2023-06-17 18:03:37 +0300

    added more fields

--- src/db.js
+++ src/db.js
@@ -4,6 +4,6 @@ import Dexie from "dexie";
 export const db = new Dexie("mainDB");
 db.version(1).stores({
   mrzEntries:
-    "&docNum, surname, givenNames, type, format, issuingOrg, nationality, sex, birthDate, expDate, personalNum, optional1, optional2, notes",
+    "&docNum, surname, givenNames, type, format, issuingOrg, nationality, sex, birthDate, expDate, personalNum, optional1, optional2, notes, createdAt, updatedAt",
 });
 // todo: add createdAt, updatedAt
