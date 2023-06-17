aa2a7d87bf91fe480b44d049342c2e2c190790d5
Ismail Saleh <centriu78@gmail.com>
2023-06-17 03:54:56 +0300

    todo

--- src/components/ButtonCreateEntry.vue
+++ src/components/ButtonCreateEntry.vue
@@ -1,3 +1,27 @@
+<!-- <template>
+  <v-btn @click="insertNewEntry">Запази</v-btn>
+</template>
+<script>
+const { MRZ_Entry } = require("../../DB/config");
+export default {
+  props: {
+    entry: Object,
+  },
+  methods: {
+    async insertNewEntry() {
+      try {
+        const newEntry = await MRZ_Entry.create(this.entry);
+        console.log("Created: ", newEntry.toJSON());
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  },
+};
+</script>
+<style></style> -->
+<!-- this doesnt work -->
+<!-- "createEntry" is not exported by "controllers/MRZ_Entry-Controller.js", imported by "src/components/ButtonCreateEntry.vue". -->
 <template>
   <v-btn @click="insertNewEntry">Запази</v-btn>
 </template>
@@ -11,7 +35,8 @@ export default {
   methods: {
     async insertNewEntry() {
       try {
-        await createEntry(this.entry);
+        const entry = await createEntry(this.entry);
+        console.log(entry);
       } catch (err) {
         console.error(err);
       }
