140a48c8a4a73d6586dc745220162b78cfeb732c
Ismail Saleh <centriu78@gmail.com>
2023-06-17 17:45:30 +0300

    add dexie

---
+++ src/db.js
@@ -0,0 +1,9 @@
+// db.js
+import Dexie from "dexie";
+
+export const db = new Dexie("mainDB");
+db.version(1).stores({
+  mrzEntries:
+    "&docNum, surname, givenNames, type, format, issuingOrg, nationality, sex, birthDate, expDate, personalNum, optional1, optional2, notes",
+});
+// todo: add createdAt, updatedAt
