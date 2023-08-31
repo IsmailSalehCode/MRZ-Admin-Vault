7616ad0eb6fefbab1799b494035049d54b5c61b8
Ismail Saleh <centriu78@gmail.com>
2023-08-31 15:02:11 +0300

    adding contact stuff

--- src/views/View_Contact.vue
+++ src/views/View_Contact.vue
@@ -1,7 +1,52 @@
 <template>
-  <div>contact</div>
+  <v-container>
+    <v-card class="mx-auto" max-width="700" rounded="lg">
+      <v-card-text>
+        <v-row>
+          <v-col cols="12" sm="4">
+            <v-img
+              max-width="200"
+              style="margin: auto; border-radius: 3%"
+              cover
+              :src="imgAuthor"
+            >
+              <template v-slot:placeholder>
+                <div class="d-flex align-center justify-center fill-height">
+                  <v-progress-circular
+                    color="blue"
+                    indeterminate
+                  ></v-progress-circular>
+                </div>
+              </template>
+            </v-img>
+          </v-col>
+          <v-col cols="12" sm="8">
+            <h2 class="py-4">Връзка със софтуерния разработчик</h2>
+            <p v-for="contact in arrContact" :key="contact.title">
+              <b>{{ contact.title }}</b
+              >: <a :href="contact.href">{{ contact.content }}</a>
+            </p>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-container>
+  <div>{{ arrContact }}</div>
 </template>
 <script>
-export default {};
+import contactAuthorChannels from "../../contact-author-channels.json";
+import imgAuthor from "../assets/images/ismail.jpg";
+
+export default {
+  data() {
+    return {
+      imgAuthor: imgAuthor,
+    };
+  },
+  computed: {
+    arrContact() {
+      return contactAuthorChannels;
+    },
+  },
+};
 </script>
-<style></style>
