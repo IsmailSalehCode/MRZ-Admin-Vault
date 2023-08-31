9e770880d4b5c6bb59b9d8cc8852d0c5117ba96e
Ismail Saleh <centriu78@gmail.com>
2023-08-31 16:34:10 +0300

    styling

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,10 @@
   <v-footer bottom absolute app theme="dark">
     <v-col>
       <v-row justify="center">
-        <v-col cols="12" sm="4" style="font-size: 0.8rem">
+        <v-col
+          cols="12"
+          style="font-size: 0.8rem; text-align: center; text-wrap: balance"
+        >
           &#169; 2023 -
           <strong>Исмаил Салех @ ТУ-София</strong>
         </v-col>
