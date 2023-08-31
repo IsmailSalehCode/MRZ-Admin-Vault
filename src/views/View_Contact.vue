09dacccfebee0885d7c55a7c10bf84ac6d86f8f6
Ismail Saleh <centriu78@gmail.com>
2023-08-31 19:38:27 +0300

    add CardCurrentReleaseInfo.vue

--- src/views/View_Contact.vue
+++ src/views/View_Contact.vue
@@ -2,13 +2,16 @@
   <v-container>
     <CardContactDeveloper />
   </v-container>
+  <v-container> <CardCurrentReleaseInfo /> </v-container>
 </template>
 <script>
 import CardContactDeveloper from "../components/CardContactDeveloper.vue";
+import CardCurrentReleaseInfo from "@/components/CardCurrentReleaseInfo.vue";
 
 export default {
   components: {
     CardContactDeveloper,
+    CardCurrentReleaseInfo,
   },
 };
 </script>
