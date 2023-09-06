8122c4378f380e097d88e4f20005aca6a9ec1b7e
Ismail Saleh <centriu78@gmail.com>
2023-09-06 19:41:17 +0300

    merge comments

--- electron/electron.js
+++ electron/electron.js
@@ -61,9 +61,7 @@ app.whenReady().then(() => {
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed, except on macOS. There, it's common for applications and their menu bar to stay active until the user quits explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
