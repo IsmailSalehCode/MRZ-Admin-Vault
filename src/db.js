70e8d57bd683e128c9ae05e91ba9df71d5952c48
Ismail Saleh <centriu78@gmail.com>
2023-09-15 19:47:13 +0300

    incr version

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
 import Dexie from "dexie";
 
 export const db = new Dexie("mainDB");
-db.version(1).stores({
+db.version(2).stores({
   mrzEntries:
     "&docNum, surname, givenNames, type, format, issuingOrg, nationality, sex, birthDate, expDate, personalNum, optional1, optional2, notes, updatedAt, collectionId",
   collections: "++id,name",
