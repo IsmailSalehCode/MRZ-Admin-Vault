68b8f809388aa2b84e12133bee6f71ac2dc10eaf
Ismail Saleh <centriu78@gmail.com>
2023-09-22 17:11:25 +0300

    remove @

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from "./App.vue";
 import { createApp } from "vue";
 
 // Plugins
-import { registerPlugins } from "@/plugins";
+import { registerPlugins } from "./plugins";
 
 const app = createApp(App);
 
